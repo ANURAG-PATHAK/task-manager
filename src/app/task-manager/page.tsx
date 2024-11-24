8a77abaa1451d1e57f4eb9e8bb60b0b0bea3f590
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-11-25 01:51:40 +0530

    updated auth

--- src/app/task-manager/page.tsx
+++ src/app/task-manager/page.tsx
@@ -1,36 +1,36 @@
-"use client"
+// "use client"
 
-import { db } from "@/firebase/firebase";
-import {
-    collection,
-    addDoc,
-    getDoc,
-    deleteDoc,
-    serverTimestamp,
-    query,
-    orderBy,
-    doc,
-    updateDoc
-} from 'firebase/firestore';
-import { useState, useEffect } from "react";
+// import { db } from "@/firebase/firebase";
+// import {
+//     collection,
+//     addDoc,
+//     getDoc,
+//     deleteDoc,
+//     serverTimestamp,
+//     query,
+//     orderBy,
+//     doc,
+//     updateDoc
+// } from 'firebase/firestore';
+// import { useState, useEffect } from "react";
 
-interface Task {
-    title: string,
-    description: string,
-    dueDate: Date
-}
+// interface Task {
+//     title: string,
+//     description: string,
+//     dueDate: Date
+// }
 
-async function addTaskToFirebase(title: string, description: string, dueDate: Date) {
-    try {
-        const docRef = await addDoc(collection(db, "tasks"), {
-            title: title,
-            description: description,
-            dueDate: dueDate,
-            createdAt: serverTimestamp()
-        });
-        return true;
-    } catch (error) {
-        console.log(`Error adding doc: ${error}`);
-        return false;
-    }
-}
+// async function addTaskToFirebase(title: string, description: string, dueDate: Date) {
+//     try {
+//         const docRef = await addDoc(collection(db, "tasks"), {
+//             title: title,
+//             description: description,
+//             dueDate: dueDate,
+//             createdAt: serverTimestamp()
+//         });
+//         return true;
+//     } catch (error) {
+//         console.log(`Error adding doc: ${error}`);
+//         return false;
+//     }
+// }
